13ceadb20272264a5d23c987fd470c5e0c566dae
Cory Burditt <26898430+coryburdit@users.noreply.github.com>
2023-07-10 16:20:11 -0500

    Changing to use new Exchange and adding .gitignore #1

--- AzureADverifydelsete.ps1
+++ AzureADverifydelsete.ps1
@@ -1,16 +1,9 @@
-################################################################################
+<################################################################################
 # Force sync to Azure AD and verify deletion                                   #
 # Author: Cory Burditt                                                         #
 # Date: March 16  , 2018                                                       #
-# Description: This script Forces a sync with Azure AD of any changes made     #
-#                 and verifies deleation of user mail box form hybrid Exchange #
-################################################################################
-Import-Module AdSync
-Start-ADSyncSyncCycle -PolicyType Delta
-Set-ExecutionPolicy RemoteSigned
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-Import-PSSession $Session
-Write-host "Enter Address to verify that is deleted."
-$mailbox = Read-Host
+# Description: This script deleation of user mail box form hybrid Exchange     #
+################################################################################>
+Connect-ExchangeOnline
+$mailbox = Read-Host -Prompt "Enter Address to verify that is deleted."
 Get-Mailbox -SoftDeletedMailbox -identity $mailbox
--- adsync.ps1
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Force sync to Azure AD                                                       #
-# Author: Cory Burditt                                                         #
-# Date: March 16  , 2018                                                       #
-# Description: This script Forces a sync with Azure AD of any changes made     #
-################################################################################
-Import-Module AdSync
-Start-ADSyncSyncCycle -PolicyType Delta
